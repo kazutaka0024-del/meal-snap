03ba9b0def24ec031b45d785021f75941e505a75
kazutaka0024-del <kazutaka0024@gmail.com>
2026-07-25 16:43:27 +0900

    Ver.0.2-2 カメラ機能追加

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-document.getElementById("cameraButton").addEventListener("click", () => {
-    alert("次回、カメラを起動します。");
+const cameraButton = document.getElementById("cameraButton");
+const cameraInput = document.getElementById("cameraInput");
+const preview = document.getElementById("preview");
+
+cameraButton.addEventListener("click", () => {
+    cameraInput.click();
+});
+
+cameraInput.addEventListener("change", (event) => {
+
+    const file = event.target.files[0];
+
+    if (!file) {
+        return;
+    }
+
+    const imageUrl = URL.createObjectURL(file);
+
+    preview.src = imageUrl;
+    preview.style.display = "block";
+
 });
